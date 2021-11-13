f5ff39c7317b6a67c9e096e45459b8e0bedc3bd2
basahota <techbasanta757@gmail.com>
2021-11-13 17:42:30 +0530

    code

---
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -0,0 +1,28 @@
+package com.javatechie.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Welcome to javatechie !";
+	}
+
+//	echo "# github-actions-example" >> README.md
+//	git init
+//	git add README.md
+//	git commit -m "first commit"
+//	git branch -M main
+//	git remote add origin https://github.com/Java-Techie-jt/github-actions-example.git
+//	git push -u origin main
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatechie/cicd/GithubCicdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
